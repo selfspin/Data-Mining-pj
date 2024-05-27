bf630d92e81e309f888032cc2b7c9ffea7618fa8
nlx0021 <nlxliwenye@126.com>
2024-05-27 19:38:52 +0800

    preprocess by lwy

---
+++ scripts/lwy.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+
+current_dir = os.getcwd()
+sys.path.append(os.path.dirname(os.path.join(current_dir, "..")))
+from utils.preprocess import *
+
+def age_stats(data):
+    pass
+
+
+def income_stats(data):
+    pass
+
+
+def career_stats(data):
+    pass
